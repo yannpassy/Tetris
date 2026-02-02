a7fae2e85f5223cc851678207cf66e49cc63330c
yannpassy <yannpassy1@gmail.com>
2026-02-02 18:15:36 +0100

    tetromino
    19:00

---
+++ Assets/Script/Tetromino.cs
@@ -0,0 +1,19 @@
+using UnityEngine.Tilemaps;
+
+public enum Tetromino
+{
+    I,
+    O,
+    T,
+    J,
+    L,
+    S,
+    Z,
+}
+
+[System.Serializable]
+public struct TetrominoData
+{
+    public Tetromino tetromino;
+    public Tile tile;
+}
