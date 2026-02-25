eabf817dffbeef378a2b7b43a8cb3d2878097e1b
yannpassy <yannpassy1@gmail.com>
2026-02-25 12:04:47 +0100

    data.cs
    22:

--- Assets/Script/Tetromino.cs
+++ Assets/Script/Tetromino.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public enum Tetromino
@@ -16,4 +17,5 @@ public struct TetrominoData
 {
     public Tetromino tetromino;
     public Tile tile;
+    public Vector2Int[] cells;
 }
