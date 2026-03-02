79412efce9ad9294505fc39927ecf5d5c3bef2f6
yannpassy <yannpassy1@gmail.com>
2026-03-02 17:45:19 +0100

    initialize piece
    35:00

--- Assets/Script/Piece.cs
+++ Assets/Script/Piece.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-    
+    public Board board {get; private set;}
+    public TetrominoData data {get; private set;}
+    public Vector3Int[] cells {get; private set;}
+    public Vector3Int position {get; private set;}
+    public void Initialize(Board _board, Vector3Int _position, TetrominoData _data)
+    {
+        this.board = _board;
+        this.position = _position;
+        this.data = _data;
+
+        if(this.cells == null)
+        {
+            this.cells = new Vector3Int[_data.cells.Length]; // which is 4 for length
+        }
+    }
 }
