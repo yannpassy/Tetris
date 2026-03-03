012094630a4d1e60d1f93bec00c5117e864aa85e
yannpassy <yannpassy1@gmail.com>
2026-03-03 14:11:11 +0100

    spawning piece
    43:00

--- Assets/Script/Board.cs
+++ Assets/Script/Board.cs
@@ -6,11 +6,14 @@ using UnityEngine.Tilemaps;
 public class Board : MonoBehaviour
 {
     public Tilemap tilemap {get; private set;}
+    public Piece activePiece {get; private set;}
     public TetrominoData[] tetrominoes;
+    public Vector3Int spawnPosition;
 
     private void Awake() 
     {
         this.tilemap = GetComponentInChildren<Tilemap>();
+        this.activePiece = GetComponent<Piece>();
         for (int i = 0; i < tetrominoes.Length; i++)
         {
             this.tetrominoes[i].Initialize();
@@ -25,11 +28,18 @@ public class Board : MonoBehaviour
     public void SpawnPiece()
     {
         int random = Random.Range(0, this.tetrominoes.Length);
-        TetrominoData data = this.tetrominoes[random];
+        TetrominoData tetromino = this.tetrominoes[random];
+
+        this.activePiece.Initialize(this, this.spawnPosition , tetromino);
+        Set(this.activePiece);
     }
 
-    public void Set()
+    public void Set(Piece _piece)
     {
-        
+        for (int i = 0; i < _piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = _piece.cells[i] + _piece.position;
+            this.tilemap.SetTile(tilePosition, _piece.tetromino.tile);
+        }
     }
 }
--- Assets/Script/Piece.cs
+++ Assets/Script/Piece.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class Piece : MonoBehaviour
 {
     public Board board {get; private set;}
-    public TetrominoData data {get; private set;}
+    public TetrominoData tetromino {get; private set;}
     public Vector3Int[] cells {get; private set;}
     public Vector3Int position {get; private set;}
-    public void Initialize(Board _board, Vector3Int _position, TetrominoData _data)
+    public void Initialize(Board _board, Vector3Int _position, TetrominoData _tetromino)
     {
         this.board = _board;
         this.position = _position;
-        this.data = _data;
+        this.tetromino = _tetromino;
 
         if(this.cells == null)
         {
-            this.cells = new Vector3Int[_data.cells.Length]; // which is 4 for length
+            this.cells = new Vector3Int[_tetromino.cells.Length]; // which is 4 for length
+        }
+
+        for (int i = 0; i < _tetromino.cells.Length; i++)
+        {
+            this.cells[i] = (Vector3Int)_tetromino.cells[i];
         }
     }
 }
